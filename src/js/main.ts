41baf187b158dcdce7c5d65276dd66bbbf1cd1c6
jenniferchristine <jeja2306@student.miun.se>
2024-04-11 13:49:34 +0200

    decleared variables in ts

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,5 @@
-interface Todo {
-    task: string;
-    completed: boolean;
-    priority: 1 | 2 | 3;
-}
+let newtodobuttonEl = document.getElementById("newtodobutton") as HTMLButtonElement;
+let messageEl: HTMLElement | null = document.getElementById("message");
+let newtodoEl = document.getElementById("newtodo") as HTMLInputElement;
+let todolistEl = document.getElementById("todolist") as HTMLDivElement; 
+let clearbuttonEl = document.getElementById("clearbutton") as HTMLButtonElement;
