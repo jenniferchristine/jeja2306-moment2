894155396fbe1383ed7786b55421d62c568f3f48
jenniferchristine <jeja2306@student.miun.se>
2024-04-11 13:57:49 +0200

    corrected to interface

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,5 @@
-let newtodobuttonEl = document.getElementById("newtodobutton") as HTMLButtonElement;
-let messageEl: HTMLElement | null = document.getElementById("message");
-let newtodoEl = document.getElementById("newtodo") as HTMLInputElement;
-let todolistEl = document.getElementById("todolist") as HTMLDivElement; 
-let clearbuttonEl = document.getElementById("clearbutton") as HTMLButtonElement;
+interface Todo {
+    task: string;
+    completed: boolean;
+    priority: 1 | 2 | 3;
+}
